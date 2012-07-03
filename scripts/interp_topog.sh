fd259c8938dfaecc61a6a4b345dfaf2ba7e3151e
Marshall Ward <marshall.ward@gmail.com>
2012-07-03 17:49:17 +1000

    Execution and running, some syntax stuff

--- scripts/interp_topog.sh
+++ scripts/interp_topog.sh
@@ -2,7 +2,7 @@
 #PBS -l walltime=0:30:00,ncpus=128,vmem=128GB
 #PBS -wd
 
-mpirun -np $PBS_NCPUS make_topog_parallel \
+mpirun -np ${PBS_NCPUS} make_topog_parallel \
     --mosaic ocean_mosaic.nc \
     --topog_type realistic \
     --topog_file etopo1.nc \
